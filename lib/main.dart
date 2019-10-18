66a576ecfea26a5dbd20f1ed123aa47f9286b55f
randrade75 <rubemandrade@hotmail.com>
2019-10-18 19:34:35 -0300

    commit2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'firestoreservice.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'taskscreen.dart';
+import 'task.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,105 +13,233 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter ToDo APP',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primaryColor: Color(0xff543B7A),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<Task> items;
+  FirestoreService fireServ = new FirestoreService();
+  StreamSubscription<QuerySnapshot> todoTasks;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+
+    items=new List();
+
+    todoTasks?.cancel();
+    todoTasks=fireServ.getTaskList().listen((QuerySnapshot snapshot){
+        final List<Task> tasks=snapshot.documents
+        .map((documentSnapshot) => Task. fromMap(documentSnapshot.data))
+        .toList();
+
+        setState(() {
+        this.items = tasks;
+      });
+      
     });
+
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+      resizeToAvoidBottomInset: false,
+      body: Column(
+        children: <Widget>[
+          _myAppBar(context),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height - 80,
+            child: ListView.builder(
+                itemCount: items.length,
+                itemBuilder: (context, index) {
+                  return Stack(children: <Widget>[
+                    // The containers in the background
+                    Column(children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(left: 8.0, right: 8.0),
+                        child: Container(
+                          width: MediaQuery.of(context).size.width,
+                          height: 80.0,
+                          child: Padding(
+                            padding: EdgeInsets.only(top: 8.0, bottom: 8.0),
+                            child: Material(
+                              color: Colors.white,
+                              elevation: 14.0,
+                              shadowColor: Color(0x802196F3),
+                              child: Center(
+                                child: Padding(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: <Widget>[
+                                      todoType('${items[index].tasktype}'),
+                                      Text(
+                                        '${items[index].taskname}',
+                                        style: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 20.0),
+                                      ),
+                                      Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: <Widget>[
+                                          Text(
+                                            '${items[index].taskdate}',
+                                            style: TextStyle(
+                                                color: Colors.black,
+                                                fontSize: 18.0,
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                          Text(
+                                            '${items[index].tasktime}',
+                                            style: TextStyle(
+                                                color: Colors.black,
+                                                fontSize: 16.0),
+                                          ),
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ]),
+                  ]);
+                }),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        backgroundColor: Color(0xFFFA7397),
+        child: Icon(
+          FontAwesomeIcons.listUl,
+          color: Color(0xFFFDDE42),
+        ),
+        onPressed: () {
+          //Navigator.push(context,MaterialPageRoute(builder: (context) => TaskScreen()),
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => TaskScreen(Task('', '', '', '', '')),
+                fullscreenDialog: true),
+          );
+        },
+      ),
+    );
+  }
+
+
+  Widget todoType(String icontype) {
+    IconData iconval;
+    Color colorval;
+    switch (icontype) {
+      case 'travel':
+        iconval = FontAwesomeIcons.mapMarkerAlt;
+        colorval = Color(0xff4158ba);
+        break;
+      case 'shopping':
+        iconval = FontAwesomeIcons.shoppingCart;
+        colorval = Color(0xfffb537f);
+        break;
+      case 'gym':
+        iconval = FontAwesomeIcons.dumbbell;
+        colorval = Color(0xff4caf50);
+        break;
+      case 'party':
+        iconval = FontAwesomeIcons.glassCheers;
+        colorval = Color(0xff9962d0);
+        break;
+      default:
+         iconval = FontAwesomeIcons.tasks;
+         colorval = Color(0xff0dc8f5);
+      //
+    }
+    return CircleAvatar(
+      backgroundColor: colorval,
+      child: Icon(iconval, color: Colors.white, size: 20.0),
+    );
+  }
+
+  Widget _myAppBar(context) {
+    return Container(
+      height: 80.0,
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+            colors: [
+              const Color(0xFFFA7397),
+              const Color(0xFFFDDE42),
+            ],
+            begin: const FractionalOffset(0.0, 0.0),
+            end: const FractionalOffset(1.0, 0.0),
+            stops: [0.0, 1.0],
+            tileMode: TileMode.clamp),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(top: 16.0),
+        child: Center(
+            child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Expanded(
+              flex: 1,
+              child: Container(
+                child: IconButton(
+                    icon: Icon(
+                      FontAwesomeIcons.arrowLeft,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {
+                      //
+                    }),
+              ),
+            ),
+            Expanded(
+              flex: 5,
+              child: Container(
+                child: Text(
+                  'ToDo Tasks',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                child: IconButton(
+                    icon: Icon(
+                      FontAwesomeIcons.search,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {
+                      //
+                    }),
+              ),
+            ),
+          ],
+        )),
+      ),
     );
   }
 }
